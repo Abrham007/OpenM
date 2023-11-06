97fab7afdfb0cdd6be1d63a986b4178dd7ed026b
Abrham007 <abrhamaraya111@gmail.com>
2023-11-06 16:05:08 +0300

    add the canister id

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,12 +3,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "./assets/home-img.png";
+import Item from "./components/Item";
 
 function App() {
+
+  const NFTID = "br5f7-7uaaa-aaaaa-qaaca-cai"
   return (
     <div className="App">
       <Header />
-      <Item />
+      <Item id={NFTID} />
       <img className="bottom-space" src={homeImage} />
       <Footer />
     </div>
