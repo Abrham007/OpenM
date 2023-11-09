5bbbd1551756df7f3496bbc27e9a12aab8c84bcd
Abrham007 <abrhamaraya111@gmail.com>
2023-11-09 15:08:34 +0300

    removed the homepage img

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "./assets/home-img.png";
+
 import Item from "./components/Item";
 import Minter from "./components/Minter";
 
 function App() {
 
-  const NFTID = "bd3sg-teaaa-aaaaa-qaaba-cai"
+  // const NFTID = "bd3sg-teaaa-aaaaa-qaaba-cai"
   return (
     <div className="App">
       <Header />
-      <Minter />
+      {/* <Minter /> */}
       {/* <Item id={NFTID} /> */}
-      <img className="bottom-space" src={homeImage} />
+      
       <Footer />
     </div>
   );
