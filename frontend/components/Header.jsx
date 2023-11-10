623ee0ba2af5a4b646b658e9faa92625d155709e
Abrham007 <abrhamaraya111@gmail.com>
2023-11-10 15:12:27 +0300

    updated the react-router to latest version

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,51 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
-import homeImage from "./assets/home-img.png";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import homeImage from "../assets/home-img.png";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Minter from "./Minter";
-import Minter from "./Gallery";
+import Gallery from "./Gallery";
 import { openm } from "../../src/declarations/openm/index.js"
+import {Principal} from "@dfinity/principal"
+import CURRENT_USER_ID from "../main.jsx";
 
 function Header() {
+
+  const [ userOwnedGallery, setOwndedGallery] = useState()
+
+  async function getNFTs() {
+    const userNFTs = await openm.getOwnedNFTs(CURRENT_USER_ID);
+    setOwndedGallery(<Gallery title={"My NFT"} ids={userNFTs} />)
+  }
+
+  useEffect(() => {
+    getNFTs();
+  },[]);
+
   return (
-    <BrowserRouter>
+    <BrowserRouter forceRefresh={true}>
     <div className="app-root-1">
       <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
         <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
           <div className="header-left-4"></div>
           <img className="header-logo-11" src={logo} />
           <div className="header-vertical-9"></div>
-            <Link to={"/"}>
+            <Link to="/">
               <h5 className="Typography-root header-logo-text">OpenD</h5>
             </Link>
           <div className="header-empty-6"></div>
           <div className="header-space-8"></div>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            <Link to={"/discover"}>Discover</Link>         
+            <Link to="/discover">Discover</Link>         
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            <Link to={"/minter"}>Minter</Link>     
+            <Link to="/minter">Minter</Link>     
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            <Link to={"/collection"}>My NFTs</Link>
+            <Link to="/collection">My NFTs</Link>
           </button>
         </div>
       </header>
     </div>
-      <Switch>
-        <Route exact path="/">
-        <img className="bottom-space" src={homeImage} />
-        </Route>
-        <Route path="/discover">
-          <h1>Discover</h1>
-        </Route>
-        <Route path="/minter">
-          <Minter />
-        </Route>
-        <Route path="/collection">
-          <Gallery title={"My NFT"}/>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<img className="bottom-space" src={homeImage}></img> }/>
+        <Route path="/discover" element={<h1>Discover</h1>} />         
+        <Route path="/minter" element={ <Minter /> } />        
+        <Route path="/collection" element={ userOwnedGallery } />    
+      </Routes>
     </BrowserRouter>
   );
 }
