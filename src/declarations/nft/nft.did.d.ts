8c7dca2e83abae6150204f98d68f1805dc229a2a
Abrham007 <abrhamaraya111@gmail.com>
2023-11-12 15:26:25 +0300

    updated the idlfactory for both canisters

--- src/declarations/nft/nft.did.d.ts
+++ src/declarations/nft/nft.did.d.ts
@@ -3,7 +3,9 @@ import type { ActorMethod } from '@dfinity/agent';
 
 export interface NFT {
   'getAsset' : ActorMethod<[], Uint8Array | number[]>,
+  'getCanisterId' : ActorMethod<[], Principal>,
   'getName' : ActorMethod<[], string>,
   'getOwner' : ActorMethod<[], Principal>,
+  'transferOwnership' : ActorMethod<[Principal], string>,
 }
 export interface _SERVICE extends NFT {}
--- src/declarations/openm/openm.did.d.ts
+++ src/declarations/openm/openm.did.d.ts
@@ -2,6 +2,9 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
 export interface _SERVICE {
+  'getOpenMCanisterID' : ActorMethod<[], Principal>,
   'getOwnedNFTs' : ActorMethod<[Principal], Array<Principal>>,
+  'isListed' : ActorMethod<[Principal], boolean>,
+  'listItem' : ActorMethod<[Principal, bigint], string>,
   'mint' : ActorMethod<[Uint8Array | number[], string], Principal>,
 }
